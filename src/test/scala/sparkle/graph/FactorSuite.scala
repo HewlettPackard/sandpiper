afe6ef9f20f8074e342b0f039c352237e0523f1c
avulanov <nashb@yandex.ru>
2016-11-17 15:11:55 -0800

    Add HPE to the headers

--- src/test/scala/sparkle/graph/FactorSuite.scala
+++ src/test/scala/sparkle/graph/FactorSuite.scala
@@ -1,4 +1,6 @@
 /*
+ * (c) Copyright 2016 Hewlett Packard Enterprise Development LP
+ *
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
